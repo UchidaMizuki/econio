c756558a638b9b43f196d6e5087b4488599a8090
Uchida Mizuki <uchidamizuki@vivaldi.net>
2024-08-21 23:43:02 +0900

    Update imports

--- R/econio-package.R
+++ R/econio-package.R
@@ -6,7 +6,9 @@
 #' @importFrom pillar tbl_format_setup
 #' @importFrom rlang .data
 #' @importFrom rlang :=
+#' @importFrom vctrs vec_cast
 #' @importFrom vctrs vec_ptype_abbr
 #' @importFrom vctrs vec_ptype_full
+#' @importFrom vctrs vec_ptype2
 ## usethis namespace: end
 NULL
