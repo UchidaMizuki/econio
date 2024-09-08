53fb2399e45a5f96161ef914cf32cdfce68a4702
Uchida Mizuki <uchidamizuki@vivaldi.net>
2024-09-08 12:48:25 +0900

    Add region.R

---
+++ R/region.R
@@ -0,0 +1,23 @@
+io_region <- function(data) {
+  dimnames <- dimnames(data)
+  union(dimnames$input$region, dimnames$output$region)
+}
+
+io_same_region <- function(data) {
+  if (inherits(data, "io_table_regional")) {
+    dibble::ones(data)
+  } else if (inherits(data, "io_table_multiregional")) {
+    region <- io_region(data)
+
+    args <- vctrs::vec_init(list(), length(region))
+    for (i in seq_along(region)) {
+      args[[i]] <- data |>
+        dplyr::filter(.data$input$region == region[[i]],
+                      .data$output$region == region[[i]]) |>
+        dibble::ones()
+    }
+    dibble::broadcast(rlang::exec(rbind, !!!args),
+                      dim_names = c("input", "output")) |>
+      tidyr::replace_na(0)
+  }
+}
