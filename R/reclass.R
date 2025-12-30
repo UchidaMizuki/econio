2239309efca5e2a731ad1d5a287c9198777e9a28
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-12-30 21:52:17 +0900

    Add io_reclass function for table reclassification

---
+++ R/reclass.R
@@ -0,0 +1,206 @@
+#' Reclassify an input-output table
+#'
+#' @param data An input-output table.
+#' @param input_region_data,input_sector_data,output_region_data,output_sector_data
+#' Data frames used to reclassify the input and output regions and sectors.
+#' If `NULL`, no reclassification is performed for that axis. By default,
+#' `NULL`.
+#' @param from_col,to_col,weight_col Names of the columns in the
+#' reclassification data. Defaults are `"from"`, `"to"`, and `"weight"`.
+#' @param weight_tolerance Tolerance for checking that weights sum to 1.
+#' By default, `.Machine$double.eps^0.5`.
+#'
+#' @return A reclassified input-output table.
+#'
+#' @export
+io_reclass <- function(
+  data,
+  input_region_data = NULL,
+  input_sector_data = NULL,
+  output_region_data = NULL,
+  output_sector_data = NULL,
+  from_col = "from",
+  to_col = "to",
+  weight_col = "weight",
+  weight_tolerance = .Machine$double.eps^0.5
+) {
+  dim_names <- dimnames(data)
+  dim_name_input <- dim_names$input
+  dim_name_output <- dim_names$output
+
+  input_sector_data <- io_reclass_axis_data(
+    axis_data = input_sector_data,
+    dim_name = dim_name_input,
+    type = "sector",
+    from_col = {{ from_col }},
+    to_col = {{ to_col }},
+    weight_col = {{ weight_col }},
+    weight_tolerance = weight_tolerance
+  )
+  output_sector_data <- io_reclass_axis_data(
+    axis_data = output_sector_data,
+    dim_name = dim_name_output,
+    type = "sector",
+    from_col = {{ from_col }},
+    to_col = {{ to_col }},
+    weight_col = {{ weight_col }},
+    weight_tolerance = weight_tolerance
+  )
+
+  if (inherits(data, "io_table_regional")) {
+    input_data <- input_sector_data |>
+      dplyr::rename_with(\(x) paste(x, "input_sector", sep = "_"), !"weight") |>
+      dibble::dibble_by(
+        to_input = "to_input_sector",
+        from_input = "from_input_sector",
+        .names_sep = "_"
+      ) |>
+      purrr::chuck("weight") |>
+      tidyr::replace_na(0)
+    output_data <- output_sector_data |>
+      dplyr::rename_with(
+        \(x) paste(x, "output_sector", sep = "_"),
+        !"weight"
+      ) |>
+      dibble::dibble_by(
+        from_output = "from_output_sector",
+        to_output = "to_output_sector",
+        .names_sep = "_"
+      ) |>
+      purrr::chuck("weight") |>
+      tidyr::replace_na(0)
+  } else if (inherits(data, "io_table_multiregional")) {
+    input_region_data <- io_reclass_axis_data(
+      axis_data = input_region_data,
+      dim_name = dim_name_input,
+      type = "region",
+      from_col = {{ from_col }},
+      to_col = {{ to_col }},
+      weight_col = {{ weight_col }},
+      weight_tolerance = weight_tolerance
+    )
+    output_region_data <- io_reclass_axis_data(
+      axis_data = output_region_data,
+      dim_name = dim_name_output,
+      type = "region",
+      from_col = {{ from_col }},
+      to_col = {{ to_col }},
+      weight_col = {{ weight_col }},
+      weight_tolerance = weight_tolerance
+    )
+
+    input_data <- tidyr::expand_grid(
+      input_region_data |>
+        dplyr::rename_with(\(x) paste(x, "input_region", sep = "_")),
+      input_sector_data |>
+        dplyr::rename_with(\(x) paste(x, "input_sector", sep = "_"))
+    ) |>
+      dplyr::mutate(
+        weight = .data$weight_input_region * .data$weight_input_sector,
+        .keep = "unused"
+      ) |>
+      dibble::dibble_by(
+        to_input = c("to_input_region", "to_input_sector"),
+        from_input = c("from_input_region", "from_input_sector"),
+        .names_sep = "_"
+      ) |>
+      purrr::chuck("weight") |>
+      tidyr::replace_na(0)
+    output_data <- tidyr::expand_grid(
+      output_region_data |>
+        dplyr::rename_with(\(x) paste(x, "output_region", sep = "_")),
+      output_sector_data |>
+        dplyr::rename_with(\(x) paste(x, "output_sector", sep = "_"))
+    ) |>
+      dplyr::mutate(
+        weight = .data$weight_output_region * .data$weight_output_sector,
+        .keep = "unused"
+      ) |>
+      dibble::dibble_by(
+        from_output = c("from_output_region", "from_output_sector"),
+        to_output = c("to_output_region", "to_output_sector"),
+        .names_sep = "_"
+      ) |>
+      purrr::chuck("weight") |>
+      tidyr::replace_na(0)
+  }
+
+  class_data <- class(data)
+  data <- data |>
+    dplyr::rename(from_input = "input", from_output = "output")
+
+  data <- input_data %*% data %*% output_data
+  data <- data |>
+    dplyr::rename(input = "to_input", output = "to_output")
+  class(data) <- class_data
+  data
+}
+
+io_reclass_axis_data <- function(
+  axis_data,
+  dim_name,
+  type,
+  from_col,
+  to_col,
+  weight_col,
+  weight_tolerance
+) {
+  axis_data_name <- rlang::as_label(rlang::enquo(axis_data))
+
+  if (is.null(axis_data)) {
+    value <- vctrs::vec_unique(dim_name[[type]])
+    axis_data <- tibble::tibble(
+      from = value,
+      to = value,
+      weight = 1
+    )
+  } else {
+    axis_data <- axis_data |>
+      dplyr::rename(
+        from = {{ from_col }},
+        to = {{ to_col }},
+        weight = {{ weight_col }}
+      )
+    weight_total <- axis_data |>
+      dplyr::summarise(weight_total = sum(.data$weight), .by = "from")
+    if (any(abs(weight_total$weight_total - 1) > weight_tolerance)) {
+      cli::cli_abort(c(
+        "{.arg {axis_data_name}} has weights that do not sum to 1 for some {.var from} values.",
+        "i" = "Check the {.var weight} column or set {.arg weight_tolerance} to a higher value if appropriate."
+      ))
+    }
+    if (type == "sector") {
+      axis_data <- axis_data |>
+        dplyr::mutate(
+          sector_type = vctrs::vec_slice(
+            io_sector_type(dim_name),
+            vctrs::vec_match(.data$from, io_sector_name(dim_name))
+          )
+        ) |>
+        dplyr::mutate(
+          from = io_sector(.data$sector_type, .data$from),
+          to = io_sector(.data$sector_type, .data$to),
+          .keep = "unused"
+        ) |>
+        tidyr::replace_na(list(weight = 1))
+    }
+
+    from_axis_data <- axis_data$from
+    axis_data <- tibble::tibble(
+      from = vctrs::vec_unique(dim_name[[type]])
+    ) |>
+      dplyr::left_join(axis_data, by = "from") |>
+      dplyr::mutate(
+        to = .data$to |>
+          dplyr::coalesce(.data$from)
+      ) |>
+      tidyr::replace_na(list(weight = 1))
+
+    if (!all(vctrs::vec_in(from_axis_data, axis_data$from))) {
+      cli::cli_abort(
+        "{.arg {axis_data_name}} has {.var from} values not present in the input-output table."
+      )
+    }
+  }
+  axis_data
+}
---
+++ tests/testthat/test-reclass.R
@@ -0,0 +1,58 @@
+test_that("io_reclass() works", {
+  names <- c(
+    "regional_noncompetitive_import",
+    "regional_competitive_import",
+    "multiregional_noncompetitive_import",
+    "multiregional_competitive_import"
+  )
+
+  for (name in names) {
+    iotable_dummy <- read_iotable_dummy(name)
+
+    expect_equal(io_reclass(iotable_dummy), iotable_dummy)
+
+    input_sector_data <- tibble::tribble(
+      ~from        , ~to            , ~weight ,
+      "industry_2" , "industry_2_1" , 0.25    ,
+      "industry_2" , "industry_2_2" , 0.75
+    )
+    input_sector_data_inverse <- input_sector_data |>
+      dplyr::rename(to = from, from = to) |>
+      dplyr::mutate(weight = 1)
+    output_sector_data <- tibble::tribble(
+      ~from        , ~to            , ~weight ,
+      "industry_3" , "industry_3_1" , 0.75    ,
+      "industry_3" , "industry_3_2" , 0.25
+    )
+    output_sector_data_inverse <- output_sector_data |>
+      dplyr::rename(to = from, from = to) |>
+      dplyr::mutate(weight = 1)
+
+    expect_equal(
+      io_reclass(
+        iotable_dummy,
+        input_sector_data = input_sector_data,
+        output_sector_data = output_sector_data
+      ) |>
+        io_reclass(
+          input_sector_data = input_sector_data_inverse,
+          output_sector_data = output_sector_data_inverse
+        ),
+      iotable_dummy
+    )
+    expect_error(
+      io_reclass(
+        iotable_dummy,
+        input_sector_data = input_sector_data |>
+          dplyr::mutate(weight = .data$weight * 2)
+      )
+    )
+    expect_error(
+      io_reclass(
+        iotable_dummy,
+        input_sector_data = input_sector_data |>
+          dplyr::mutate(from = paste(.data$from, "x", sep = "_"))
+      )
+    )
+  }
+})
